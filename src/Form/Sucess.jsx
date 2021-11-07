1b8db108e519cc329721204faf52ce3947b9350a
Matthew Magnotta <matthewmagnotta@Matthews-MacBook-Pro.local>
2021-11-07 10:58:56 -0800

    refactor: move svg to its own file

--- src/Form/Sucess.jsx
+++ src/Form/Sucess.jsx
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/core';
-
+import { CheckmarkSvg } from '../Assets/Checkmark';
 const useStyles = makeStyles(() => {
   return {
     container: {
@@ -14,28 +14,9 @@ const Success = () => {
   return (
     <div className={classes.container}>
       <div>
-        {/* animation handled in app.css */}
-        <svg
-          id="Capa_1"
-          data-name="Capa 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 513 497.33"
-          width="10rem"
-        >
-          <path
-            class="cls-1"
-            className="path"
-            d="M474.05,173.81a8,8,0,0,0-5.13,10.09,232.84,232.84,0,1,1-50.49-85.74,8,8,0,1,0,11.73-10.88,247.32,247.32,0,1,0,54,91.66A8,8,0,0,0,474.05,173.81Z"
-            transform="translate(0.5 -7.33)"
-          />
-          <path
-            class="cls-1"
-            className="path"
-            d="M505,83.26a24,24,0,0,0-34,0l-7.16,7.16a8,8,0,1,0,11.31,11.31l7.17-7.16a8,8,0,1,1,11.32,11.33L217.8,381.75a8,8,0,0,1-11.32,0L98.75,274a8,8,0,1,1,11.32-11.33l96.42,96.41a8,8,0,0,0,11.31,0L452.65,124.25a8,8,0,1,0-11.31-11.31L212.14,342.13l-90.75-90.75a24,24,0,1,0-34,33.95L195.16,393.06a24,24,0,0,0,34,0L505,117.21a24,24,0,0,0,0-33.95Z"
-            transform="translate(0.5 -7.33)"
-          />
-        </svg>
+        <CheckmarkSvg />
       </div>
+
       <h1>Thank you!</h1>
       <p>Your submission has been sent.</p>
     </div>
